57e4f3a42878f1ee1cced96083fd88747f5d82e2
julienmaster21 <julienmaster21@gmail.com>
2019-12-24 23:35:14 +0100

    puzzle 1

---
+++ day 1/puzzle 1/main.py
@@ -0,0 +1,53 @@
+# Imports
+import numpy as np
+
+
+# Define Module class
+class Module:
+
+    def __init__(self, mass):
+        self.mass = mass
+        self.fuelRequirement = int
+
+
+# Initialise the classes list
+modules = []
+
+# Retrieve the mass of modules from input.txt
+moduleInput = open("input.txt", "r")
+
+# Add all modules to the modules list
+currentMass = 0
+line = moduleInput.readline()
+while line != "":
+    currentMass = int(line)
+    modules.append(Module(currentMass))
+    line = moduleInput.readline()
+
+# Calculate fuel requirement
+currentFuelRequirement = 0
+for module in modules:
+    # Take the mass
+    currentFuelRequirement = module.mass
+
+    # Divide by 3
+    currentFuelRequirement /= 3
+
+    # Round down
+    currentFuelRequirement = np.floor(currentFuelRequirement)
+
+    # Subtract 2
+    currentFuelRequirement -= 2
+
+    # Turn to int
+    currentFuelRequirement = int(currentFuelRequirement)
+
+    # Assign fuel requirement
+    module.fuelRequirement = currentFuelRequirement
+
+# Get the sum of all the fuel requirements
+totalFuelRequirement = 0
+for module in modules:
+    totalFuelRequirement += module.fuelRequirement
+
+print(totalFuelRequirement)
